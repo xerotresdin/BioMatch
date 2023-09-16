4dbaa480d86b7d58b368b80bc6e045341cf172fc
bathan <side_nathan@hotmail.com>
2023-09-16 17:26:56 -0400

    Change index.js to include more props in data json

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,35 @@ app.use(express.json());
 app.get("/api/data", (req, res) => {
   const testData = [
     {
+      "id": 1,
       "name": "Phase 1 Clinical Trial",
-      "id": 1
+      "Description": "Testing drug X in the Hispanic population",
+      "incomePref": 40000,
+      "sexPref": "M",
+      "racePref": "Hispanic",
+      "medicalHistoryPref": null,
+      "timeCommitmentWeeks": 3
+
     },
     {
+      "id": 2,
       "name": "Phase 2 Clinical Trial",
-      "id": 2
+      "Description": "Looking for female patients who have undergone chemotherapy for Breast Cancer",
+      "incomePref": 10000,
+      "sexPref": "F",
+      "racePref": null,
+      "medicalHistoryPref": "Breast Cancer",
+      "timeCommitmentWeeks": 8
     },
     {
+      "id": 3,
       "name": "Phase 3 Clinical Trial",
-      "id": 3
+      "Description": "Testing new antidepressant.",
+      "incomePref": 40000,
+      "sexPref": "M",
+      "racePref": null,
+      "medicalHistoryPref": "Clinical Depression",
+      "timeCommitmentWeeks": 12
     }
   ];
   
