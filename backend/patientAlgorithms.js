54aaded7b4ca1b28828afdee94597b0d86f6e0a2
lilchromozome <122000787+lilchromozome@users.noreply.github.com>
2023-09-16 18:39:01 -0400

    basic check finished

--- backend/patientAlgorithms.js
+++ backend/patientAlgorithms.js
@@ -47,10 +47,51 @@ class Patient{
 
 }
 
+
+
+//range compare
+function range_compare(range, target){
+    switch(range){
+        case range[0] == null && range[1] == null:
+            break;
+        case range[0] == null && range[1] != null:
+            if(target > range[1])
+            return false;
+            break;
+        case range[0] != null && range[1] == null:
+            if(target < range[0])
+            return false;
+            break;
+        case range[0] != null && range[1] != null:
+            if(target < range[0] || target > range[1]){
+                return false;
+            }
+            break;
+    }
+    return true;
+}
+//set compare
+function set_compare(desired_arr, target_arr){
+    let status = false;
+    const new_set = new Set(desired_arr);
+    for (const element of target_arr) {
+        if (new_set.has(element)) {
+            status = true; // Found a common element
+        }
+    }
+    return status;
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 class Institution{
-    constructor() {
-        this.required_params = new Patient(sex, age, race, medical_history, income_index);
-        this.desired_params = new Patient(sex, age, race, medical_history, income_index);
+    /**
+     * 
+     * @param {*} required:= patient object of things that are must, !!!!! all params are arrays!!!!!
+     * @param {*} desired:= same as above
+     */
+    constructor(required, desired) {
+        this.required = required;
+        this.desired = desired;
         }
     /**
      * 
@@ -61,57 +102,31 @@ class Institution{
      * @param {*} income_index_range:= required sociostatus [min, max]
      */
     set_required_params(sex, age_range, race_range, medical_history, income_index_range){
-        this.required_params(sex, age_range, race_range, medical_history, income_index_range);
+        this.required = new Patient(sex, age_range, race_range, medical_history, income_index_range);
     }
-    set_desired_params(sex, age_range, race, medical_history, income_index){
-        this.required_params(sex, age_range, race_range, medical_history, income_index_range);
+    set_desired_params(sex, age_range, race_range, medical_history, income_index_range){
+        this.desired = new Patient(sex, age_range, race_range, medical_history, income_index_range);
     }
 
     /** for required paraneters
-     * @param {*} required_params is a patient class
+     * @param {*} required is a patient class
      */
-    basic_check(target, required_params){
+    basic_check(target){
         if(target instanceof Patient){
             //sex
-            if(this.required_params.sex != null && this.required_params.sex != target.sex){ return false;}
-            //age range
-            const age = this.required_params.age;
-            switch(age){
-                case age[0] == null && age[1] == null:
-                    break;
-                case age[0] == null && age[1] != null:
-                    if(target.age > age[1])
-                    return false;
-                    break;
-                case age[0] != null && age[1] == null:
-                    if(target.age < age[0])
-                    return false;
-                    break;
-                case age[0] != null && age[1] != null:
-                    if(target.age < age[0] || target.age > age[1]){
-                        return false;
-                    }
-                    break;
-            }
-             //income_index range
-             const income = this.required_params.income_index;
-             switch(income){
-                 case income_index[0] > target.income_index:
-                     return false;
-                 case income_index[1] < target.income_index:
-                     return false;
-             }
-            //race
-            let race_status = true;
-            for (const element of this.required_params.race) {
-                if (target.race.has(element)) {
-                  race_status = true; // Found a common element
-                }
-            }
-            
+            if(this.required.sex != null && this.required.sex != target.sex){ return false;}
+            //age compare
+            if(!range_compare(this.required.age, target.age)){ return false;}
+            //income compare
+            if(!range_compare(this.required.income_index, target.income_index)){return false;}
+            //race compare
+            if(!set_compare(this.required.race, target.race)){return false;}
+            //medical history
+            if(!set_compare(this.required.medical_history, target.medical_history)){return false;}
         } else{
             throw new Error('Invalid argument type: arg not patient')
         }
+        return true;
     }
 
 /** for desired parameters */
@@ -119,11 +134,24 @@ class Institution{
 }
 
 
+function printpat(a){
+    console.log(a.sex);
+    console.log(a.age);
+    console.log(a.race);
+    console.log(a.medical_history);
+    console.log(a.income_index);
+}
 
+const x = new Patient ("male", 5,["black"], ["fever", "high bp"], 3);
+const required = new Patient ("male", [3, null], ["black"], ["fever", "high bp"], 3);
+const desired = new Patient(null, 3, ["black", "mexican"]);
+const a = new Institution(required, desired);
 
-const x = new Patient ("male", 5, "black", ["fever", "high bp"], 3);
-console.log(x.get_income());
-x.add_medical_history("balls");
-console.log(x.medical_history);
-x.remove_medical_history("balls")
-console.log(x.medical_history);
+// printpat(desired);
+console.log(a.basic_check(x));
+
+// console.log(x.get_income());
+// x.add_medical_history("balls");
+// console.log(x.medical_history);
+// x.remove_medical_history("balls")
+// console.log(x.medical_history);
