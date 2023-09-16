233ad4c73d5766fc4b8845cc276c317f14620b2c
bathan <side_nathan@hotmail.com>
2023-09-16 18:27:34 -0400

    Implement ClinicalListing mapping in App.jsx

--- frontend/src/components/ClinicalListing.jsx
+++ frontend/src/components/ClinicalListing.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ClinicalListing(props) {
   return (
-    <div className="flex border border-gray-300 w-2/3 p-4">
+    <div className="flex border border-red-300 w-full p-4">
       <div>
         {props.trial.name}
       </div>
