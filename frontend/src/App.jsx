d142477513b16a520aa130fc773214ab341a50b9
xerotresdin <wli111@jh.edu>
2023-09-17 03:54:06 -0400

    fixed image rendering and debugged serach bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,9 +33,9 @@ function App() {
     <BrowserRouter>
     <div className="flex flex-col h-screen bg-gray-150 py-4">
       <div className="flex flex-row justify-between items-center"><div>
-      <img className="ml-4 max-w-9 max-h-9 rounded-lg" src="../images/BioMatchLogo.jpg" alt="BioMatch Logo" />
+      <img className="ml-14 max-w-9 max-h-9 rounded-lg" src="../images/BioMatchLogo.jpg" alt="BioMatch Logo" />
         </div>
-        <div className="absolute left-0 top-1 text-3xl font-bold py-4 px-16 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+        <div className="absolute left-9 top-1 text-3xl font-bold py-4 px-16 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
           Biomatch</div>
           <Link to="/signup-login">
           <button className="bg-blue-500 hover:bg-blue-400 text-white text-xs font-sans py-2 px-3 border-b-4 border-blue-700 hover:border-blue-500 rounded m-1">
@@ -45,10 +45,10 @@ function App() {
       </div>
       <hr className="h-px my-2 bg-gray-200 border-0 " />
 
-      <div className="text-4xl font-semibold font-sans text-black-900 py-6 pb-1 px-16">
+      <div className="text-4xl font-semibold text-black-900 py-6 pb-1 px-16">
         Find the perfect trial for you</div>
 
-      <div className="fonts-sans text-black-900 py-1 pb-12 px-16 text-opacity-custom">
+      <div className="fonts-sans text-black text-opacity-60 py-1 pb-12 px-16">
         Looking for trials? Browse through the latest clinical trials to find and apply for the one that best suits you!</div>
 
       <hr className="h-px bg-gray-200 border-0 " />
