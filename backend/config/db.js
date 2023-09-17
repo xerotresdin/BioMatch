e8b6b8f9402f9c9b62b04664bb282472ac6634b0
bathan <side_nathan@hotmail.com>
2023-09-16 23:49:09 -0400

    Add Nahuel's clinical trial comparison function

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: __dirname + "../../.env" });
+require("dotenv").config({ path: __dirname + "/../../.env" });
 
 const connectToDatabase = async () => {
   try {
     const connection = await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@main.ruuxroy.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log(`Connected to MongoDB at ${connection.connection.host}`);
+    console.log(`Connected to MongoDB at ${mongoose.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,18 +1,68 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const ClinicalTrial = require("../models/ClinicalTrial");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require("dotenv").config({ path: path.join(__dirname, "../../.env")});
+require("dotenv").config({ path: path.join(__dirname, "/../../.env")});
 
-const secretKey = process.env.
+const secretKey = process.env.JWT_SECRET_KEY;
+
+function calculateCompatibility(user, clinicalTrials) {
+  const age = user.age;
+  const sex = user.sex;
+  const race = user.sex;
+  const medicalHistory = user.medicalHistory;
+  const incomeIndex = user.incomeIndex;
+
+  const trialScores = [];
+
+  for (const trial of clinicalTrials) {
+    let score = 0;
+
+    if (age >= trial.ageRange[0] && age <= trial.ageRange[1]) {
+      score += trial.ageRange[2];
+    }
+
+    if (trial.patientSex[0] === "M" && sex === 1) {
+      score += trial.patientSex[1];
+    } else if (trial.patientSex[0] === "F" && sex === 2) {
+      score += trial.patientSex[1];
+    }
+
+    if (trial.studyRace.includes(race[0])) {
+      score += trial.studyRace[1];
+    }
+
+    for (const condition of trial.patientMedicalHistory) {
+      if (medicalHistory.includes(condition.condition)) {
+        score += condition.significance;
+      }
+    }
+
+    if (incomeIndex >= trial.patientIncome[0] && incomeIndex <= trial.patientIncome[1]) {
+      score += trial.patientIncome[2];
+    }
+
+    trialScores.push({ trialName: trial.name, score, details: trial });
+  }
+
+  trialScores.sort((a, b) => b.score - a.score);
+  
+  return trialScores;
+}
 
 router.get("/data", async (req, res) => {
   try {
-    const clinicalTrials = await ClinicalTrial.find({});
+    const currentUser = req.body.user;
+    console.log(currentUser);
+    let clinicalTrials = await ClinicalTrial.find({});
     console.log(clinicalTrials);
-    res.json(clinicalTrials);
+
+    clinicalTrials = calculateCompatibility(currentUser, clinicalTrials);
+    console.log(clinicalTrials);
+    res.status(200).json(clinicalTrials);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -64,12 +114,26 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-router.post("/signin", (req, res) => {
+router.post("/signin", async (req, res) => {
   const { username, password } = req.body;
   try {
-    
-  } catch (error) {
+    const foundUser = await User.findOne({ username });
 
+    if (!foundUser) {
+      res.status(400).json({ message: "No such username exists." });
+      return;
+    }
+
+    const validPassword = bcrypt.compare(password, foundUser.password);
+
+    if (validPassword) {
+      const token = jwt.sign({ username: foundUser.username }, secretKey, { expiresIn: '1h' });
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: "Invalid password. "});
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error." });
   }
 });
 
