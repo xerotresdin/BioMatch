88d259128024371266adf32f5db52f8845987c9e
bathan <side_nathan@hotmail.com>
2023-09-17 00:18:24 -0400

    Update models to make certain fields required

--- backend/models/ClinicalTrial.js
+++ backend/models/ClinicalTrial.js
@@ -1,19 +1,46 @@
 const mongoose = require("mongoose");
 
 const clinicalTrialSchema = new mongoose.Schema({
-  name: String,
-  institution: String,
-  description: String,
-  compensation: Number,
-  ageRange: [Number, Number, Number, Boolean],
-  patientSex: [String, Number, Boolean],
-  studyRace: [String, Number, Boolean],
-  patientMedicalHistory: [{
-    condition: String,
-    significance: Number,
-    isRequired: Boolean
-  }],
-  patientIncome: [Number, Number, Boolean]
+  name: {
+    type: String,
+    required: true
+  },
+  institution: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  compensation: {
+    type: Number,
+    required: true
+  },
+  ageRange: {
+    type: [Number, Number, Number, Boolean],
+    required: true
+  },
+  patientSex: {
+    type: [String, Number, Boolean],
+    required: true
+  },
+  studyRace: {
+    type: [String, Number, Boolean],
+    required: true
+  },
+  patientMedicalHistory: {
+    type: [{
+      condition: String,
+      significance: Number,
+      isRequired: Boolean
+    }],
+    required: true
+  },
+  patientIncome: {
+    type: [Number, Number, Boolean],
+    required: true
+  }
 });
 
 const ClinicalTrial = mongoose.model("ClinicalTrial", clinicalTrialSchema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,30 @@
 const { default: mongoose } = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  email: String,
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phoneNumber: String, // This field is optional and not required
   verified: Boolean,
-  sex: String,
-  age: Number,
-  race: [String],
-  medicalHistory: [String],
-  incomeIndex: Number
+  sex: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  race: {
+    type: [String],
+    required: true
+  },
+  medicalHistory: [String], // This field is optional and not required
+  incomeIndex: Number // This field is optional and not required
 }); 
 
 const User = mongoose.model("User", userSchema);
