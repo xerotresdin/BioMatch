e8988b79770203b6d36cadde50ac460bdffadb85
bathan <side_nathan@hotmail.com>
2023-09-16 23:01:01 -0400

    Finish basic user authentication

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -36,14 +36,31 @@ router.post("/data", async (req, res) => {
   }
 });
 
-router.post("signup", async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
   try {
+    const foundUser = await User.findOne({ username });
+
+    if (foundUser) {
+      res.status(400).json({ message: "Username already exists." });
+      return;
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username: username,
+      password: hashedPassword,
+      ...req.body
+    });
+    console.log(newUser);
+
+    await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ message: "An error occurred while registering user." });
   }
 });
+
+router.post("/signin")
 
 module.exports = router;
