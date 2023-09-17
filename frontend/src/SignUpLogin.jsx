09ad14aa3e33c91f95b6a9ae5a7df03504a4b938
xerotresdin <wli111@jh.edu>
2023-09-17 05:42:14 -0400

    Implemented login and sign-up page

--- frontend/src/SignUpLogin.jsx
+++ frontend/src/SignUpLogin.jsx
@@ -1,51 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import 'tailwindcss/tailwind.css';
 
 function SignUpLogin() {
   const [isLoginForm, setIsLoginForm] = useState(true);
-
   const toggleForm = () => {
     setIsLoginForm(!isLoginForm);
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-indigo-500">
-      <div className="w-[390px] bg-white p-10 rounded-2xl shadow-xl text-center">
-        <div className={`title ${isLoginForm ? 'text-blue-500' : 'text-black'}`}>
-          {isLoginForm ? 'Login Form' : 'Signup Form'}
+    <div className="flex flex-row justify-center items-center h-screen">
+      <div className="w-[390px] p-10 rounded-2xl shadow-2xl text-center">
+        <div className="flex flex-row px-12 items-center mt-4"><div className="mr-2">
+          <img className="max-w-9 max-h-9 rounded-lg" src="../images/BioMatchLogo.jpg" alt="BioMatch Logo" />
         </div>
-        <div className="flex justify-between mt-5">
-          <label htmlFor="login" className={`slide cursor-pointer relative ${isLoginForm ? 'text-blue-500' : 'text-black'}`}>
-            <span className="relative z-10">Login</span>
-            <div className={`absolute inset-0 rounded-lg bg-gradient-to-r ${isLoginForm ? 'from-blue-500 to-indigo-500' : 'from-black to-indigo-700'}`}></div>
-          </label>
-          <label htmlFor="signup" className={`slide cursor-pointer relative ${isLoginForm ? 'text-black' : 'text-blue-500'}`}>
-            <span className="relative z-10">Signup</span>
-            <div className={`absolute inset-0 rounded-lg bg-gradient-to-r ${isLoginForm ? 'from-black to-indigo-700' : 'from-blue-500 to-indigo-500'}`}></div>
-          </label>
+          <div className="m-1 text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            Biomatch</div>
         </div>
         <div className="relative mt-5">
           <input type="radio" name="slide" id="login" checked={isLoginForm} readOnly className="hidden" />
           <input type="radio" name="slide" id="signup" checked={!isLoginForm} readOnly className="hidden" />
           <div className="slider-tab absolute h-full w-[50%] top-0 left-0 bg-gradient-to-r from-blue-500 to-indigo-500 rounded-2xl transition-transform duration-300"></div>
         </div>
+
+
         <form className={isLoginForm ? '' : 'hidden'} action="#">
-          <div className="field mt-5">
+          <div className="field mt-10">
             <input type="text" placeholder="Email Address" required className="w-full p-4 border border-gray-300 rounded-lg" />
           </div>
           <div className="field mt-5">
             <input type="password" placeholder="Password" required className="w-full p-4 border border-gray-300 rounded-lg" />
           </div>
           <div className="pass-link text-center mt-5">
-            <a href="#" className="text-blue-500">Forgot password?</a>
+            <a href="#" className="text-blue-500">
+
+              Forgot password?</a>
           </div>
+
+
           <div className="field btn relative mt-5">
-            <div className="btn-layer absolute top-0 left-0 w-full h-full bg-gradient-to-r from-blue-500 to-indigo-500 border border-blue-700 rounded-lg transition-all duration-300"></div>
+            <div className="btn-layer absolute top-0 left-0 w-full p-4 bg-gradient-to-r from-blue-500 to-indigo-500 border border-blue-700 rounded-lg 
+              transition-all duration-300">
+              <a href="#" onClick={toggleForm} className="font-semibold text-2xl text-white">
+                Login
+              </a>
+            </div>
             <input type="submit" value="Login" className="z-10 w-full h-full p-4 text-white bg-transparent border-none rounded-lg cursor-pointer" />
           </div>
           <div className="signup-link text-center mt-5">
-            Create an account <a href="#" onClick={toggleForm} className="text-blue-500">Sign up now</a>
+
+            Create an account <a href="#" onClick={toggleForm} className="text-blue-500">
+
+              Sign up now</a>
           </div>
+
+
         </form>
+
+
         <form className={isLoginForm ? 'hidden' : ''} action="#">
           <div className="field mt-5">
             <input type="text" placeholder="Name" required className="w-full p-4 border border-gray-300 rounded-lg" />
@@ -60,11 +71,19 @@ function SignUpLogin() {
             <input type="password" placeholder="Confirm password" required className="w-full p-4 border border-gray-300 rounded-lg" />
           </div>
           <div className="field btn relative mt-5">
-            <div className="btn-layer absolute top-0 left-0 w-full h-full bg-gradient-to-r from-blue-500 to-indigo-500 border border-blue-700 rounded-lg transition-all duration-300"></div>
-            <input type="submit" value="Signup" className="z-10 w-full h-full p-4 text-white bg-transparent border-none rounded-lg cursor-pointer" />
+
+            <div className="btn-layer absolute top-0 left-0 w-full p-4 bg-gradient-to-r from-blue-500 to-indigo-500 border border-blue-700 rounded-lg 
+              transition-all duration-300">
+              <a href="#" onClick={toggleForm} className="font-semibold text-2xl text-white">
+                Login
+              </a>
+            </div>
           </div>
+          <input type="submit" value="Signup" className="z-10 w-full h-full p-4 text-white bg-transparent border-none rounded-lg cursor-pointer" />
           <div className="signup-link text-center mt-5">
-            Already have an account? <a href="#" onClick={toggleForm} className="text-blue-500">Login</a>
+            Already have an account? <a href="#" onClick={toggleForm} className="text-blue-500">
+
+              Login</a>
           </div>
         </form>
       </div>
