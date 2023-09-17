cbc473eed68bc1c2dadc852b71856a787f8f0261
lilchromozome <122000787+lilchromozome@users.noreply.github.com>
2023-09-17 01:17:27 -0400

    sliderrr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,7 +61,7 @@ function App() {
             <input type="checkbox" class="form-checkbox h-5 w-5 text-indigo-600"></input>
             <span className="ml-2 text-gray-700"> Female</span>
           </label>
-          <div className="py-12 pb-4">Race</div>
+          <div className="py-18 pb-4">Race</div>
           <div className="flex w-1/2 items-center grid-cols-2 gap-1">
             <div className="w-1/2">
               <input type="checkbox" className="form-checkbox h-5 w-5 text-indigo-600"></input>
@@ -77,6 +77,21 @@ function App() {
               <div className="translate-y-[-0.15em] text-gray-700 whitespace-nowrap"> Native American</div>
               <div className="text-gray-700"> White</div>
             </div>
+          </div>
+          <div className="pt-4">Compensation Rate</div>
+          <div className="flex translate-x-[-5rem] py-3 grid-cols-3">   
+            <div className = "w-1/2"></div>
+            <div className="text-gray-700 px-2"> min</div>
+            <input type ="textbox" className="w-12"></input>
+            <div className = "w-1/3"></div>
+            <div className="text-gray-700 px-2"> max</div>
+            <input type ="textbox" className="w-12"></input>
+          </div>
+          <div>
+            <div className = "py-2">Age:</div>
+            <input type = "range" className="h-1.5 w-full bg-blue-200 rounded-lg " min = "0" max = "100" step = "1" id ="slider" value={sliderValue}
+        onChange={handleSliderChange}></input>
+            <output for="slider" class="text-xl mt-2 text-center">50</output>
           </div>
         </div>
         <div className="w-3/4 p-4 bg-gray-300">
