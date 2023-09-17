b29577365388dbb2f56b4c6714c8b7e686ef30ea
lilchromozome <122000787+lilchromozome@users.noreply.github.com>
2023-09-16 21:55:07 -0400

    maf

--- backend/patientAlgorithms.js
+++ backend/patientAlgorithms.js
@@ -1,4 +1,3 @@
-
 /**
  * sex: binary value (M/F)
  * age: single number
@@ -47,8 +46,6 @@ class Patient{
 
 }
 
-
-
 //range compare
 function range_compare(range, target){
     if (range[0] == null && range[1] == null){
@@ -84,10 +81,10 @@ class Institution{
      * @param {*} desired:= same as above
      * @param {*} significance_arr:= array that is 5 elements long, contains integers from 0-10
      */
-    constructor(required, desired, significance_arr) {
-        this.required = required;
-        this.desired = desired;
-        this.significance = significance_arr;
+    constructor() {
+        this.required;
+        this.desired;
+        this.significance;
         }
     /**
      * 
@@ -97,11 +94,14 @@ class Institution{
      * @param {*} medical_history:= required medical conditionsie: [highbp, stroke, ...]
      * @param {*} income_index_range:= required sociostatus [min, max]
      */
-    set_required_params(sex, age_range, race_range, medical_history, income_index_range){
+    set_required_params(sex = null, age_range = [null, null], race_range = [null, null], medical_history = [null], income_index_range = [null]){
         this.required = new Patient(sex, age_range, race_range, medical_history, income_index_range);
     }
-    set_desired_params(sex, age_range, race_range, medical_history, income_index_range){
+    set_desired_params(sex = null, age_range = [null, null], race_range = [null, null], medical_history = [null], income_index_range = [null]){
         this.desired = new Patient(sex, age_range, race_range, medical_history, income_index_range);
+    }
+    set_significance_array(arr){
+        this.significance = arr;
     }
 
     /** for required paraneters
@@ -130,7 +130,10 @@ class Institution{
      * @param {*} target is a patient class
      * returns true or false value
      */
-    fitness_check(target, significance_arr){
+    fitness_check(target, significance_arr = [0,0,0,0,0]){
+        if(significance_arr == [0,0,0,0,0]){
+            return 1;
+        }
         let maxpoints = 0;
         for(let i = 0; i < significance_arr.length; i++){
             maxpoints += significance_arr[i];
@@ -138,24 +141,24 @@ class Institution{
         let points_gained = 0;
         if(target instanceof Patient){
             //sex
-            if(this.desired.sex != null && this.required.sex != target.sex){
-                points_gained += significance_arr[0];}
+            if(this.desired.sex != null && this.desired.sex != target.sex){
+                points_gained += significance_arr[0] * .1;}
             //age compare
             if(!range_compare(this.desired.age, target.age)){
-                points_gained += significance_arr[1];}
+                points_gained += significance_arr[1] * .1;}
             //income compare
             if(!range_compare(this.desired.income_index, target.income_index)){
-                points_gained += significance_arr[2];}
+                points_gained += significance_arr[2] * .1;}
             //race compare
             if(!set_compare(this.desired.race, target.race)){
-                points_gained += significance_arr[3];}
+                points_gained += significance_arr[3] * .1;}
             //medical history
             if(!set_compare(this.desired.medical_history, target.medical_history)){
-                points_gained += significance_arr[4];}
+                points_gained += significance_arr[4] * .1;}
         } else{
             throw new Error('Invalid argument type: arg not patient')
         }
-        return points_gained/maxpoints;
+        return points_gained;
     }
 }
 
@@ -169,14 +172,15 @@ function printpat(a){
 }
 
 const x = new Patient ("male", 5,["black"], ["fever", "high bp"], 3);
-const required = new Patient ("male", [null, 5], ["black", "Mexican"], ["fever", "high bp", "hon"], [0,4]);
-const desired = new Patient(null, 3, ["black", "mexican"]);
 const sigarr = [0, 1, 3, 4, 5];
-const a = new Institution(required, desired);
+const a = new Institution(sigarr);
+a.set_desired_params(null, [1,5]);
+a.set_required_params("male", [null, 5], ["black", "Mexican"], ["fever", "high bp", "hon"], [0,4]);
+a.set_significance_array(sigarr);
 
 // printpat(desired);
 console.log(a.basic_check(x));
-console.log(a.fitness_check(x));
+console.log(a.fitness_check(x, sigarr));
 
 // console.log(x.get_income());
 // x.add_medical_history("balls");
