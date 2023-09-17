12d1ed93d3cf2e8ecce1f216b5b39df8f6e98ab9
xerotresdin <wli111@jh.edu>
2023-09-16 22:06:31 -0400

    branch fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,60 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import SearchBar from './components/SearchBar';
-import ClinicalListing from "./components/ClinicalListing"
-import 'tailwindcss/tailwind.css';
-import axios from "axios";
+  import React, { useState, useEffect } from 'react';
+  import SearchBar from './components/SearchBar';
+  import ClinicalListing from "./components/ClinicalListing";
+  import 'tailwindcss/tailwind.css';
+  import axios from "axios";
 
-function App() {
-  const [fetchedTrials, setFetchedTrials] = useState([]);
-  const [clinicalTrials, setClinicalTrials] = useState([]);
-  const [query, setQuery] = useState('');
+  function App() {
+    const [fetchedTrials, setFetchedTrials] = useState([]);
+    const [clinicalTrials, setClinicalTrials] = useState([]);
+    const [query, setQuery] = useState('');
 
-  // Function to handle search
-  const handleSearch = (searchQuery) => {
-    // Implement search logic here
+    // Function to handle search
+    const handleSearch = (searchQuery) => {
+      // Implement search logic here
+      console.log('Searching for:', searchQuery);
+      // Update clinicalTrials based on the search result
+      setClinicalTrials([]);
+    };
 
-    console.log('Searching for:', searchQuery);
-    // Update clinicalTrials based on the search result
+    useEffect(() => {
+      // Fetch data from the server
+      axios.get('http://localhost:3001/api/data')
+        .then(response => {
+          // Assuming response.data is an array of clinical trials
+          setFetchedTrials(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    }, []);
 
-    setClinicalTrials([]);
-  };
-
-  useEffect(() => {
-    // Fetch data from the server
-    axios.get('http://localhost:3001/api/data')
-      .then(response => {
-        // Assuming response.data is an array of clinical trials
-        setFetchedTrials(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
-  return (
-    <div className="flex flex-col h-screen">
-      <div className="text-5xl font-bold py-3 px-12 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-           Biomatch </div>
-      <div className="text-1xl font- bg-gray-200 text-black-900 py-3 px-12">
-          Find the perfect trial for you </div>
-      <div className="text-0.5xl font-normal bg-gray-200 text-black-900 py-3 px-12">
-          Looking for trials? Browse the latest clinical trial openings to view & apply to the best trial for you</div>
-      <div className="flex flex-row h-full">
-        <div className="w-1/4 p-4 bg-gray-200">
-          Left Container (1/4 of viewport)
+    return (
+      <div className="flex flex-col h-screen bg-gray-180">
+        <div className="flex flex-row">
+          <img className="h-auto max-w-full" src="./assets/images/BioMatchLogo.jpg"/>
+          <div className="text-3xl font-bold py-3 px-12 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            Biomatch
+          </div>
         </div>
-        <div className="w-3/4 p-4 bg-gray-300">
-          <ul>
-            {fetchedTrials.map((trial) => {
-              return (
-                <li key={trial.id}><ClinicalListing trial={trial}/></li>
-              )
-            })}
-          </ul>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-200" />
+        <div className="text-4xl font-semibold font-sans bg-gray-180 text-black-900 py-6 px-16">
+          Find the perfect trial for
+        </div>
+        <div className="text-0.5xl fonts-sans bg-gray-180 text-black-900 py-4 px-16">
+          Looking for trials? Browse the latest clinical trial openings to view & apply to the best trial for you
+        </div>
+        <hr className="h-px bg-gray-200 border-0 dark:bg-gray-300" />
+        <div className="flex flex-row h-full">
+          <div className="px-16 py-9 w-1/4 p-4 font-semibold  bg-gray-200">
+            Filter:
+          </div>
+          <div className="w-3/4 p-4 bg-gray-300">
+            <ul>
+              {fetchedTrials.map((trial) => {
+                return (
+                  <li key={trial.id}><ClinicalListing trial={trial}/></li>
+                )
+              })}
+            </ul>
+            <SearchBar onSearch={handleSearch} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default App;
+  export default App;
